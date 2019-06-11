f4a653cd24d1b264850f2b23c340269dad743aed
e <1303249523@qq.com>
2019-06-11 14:33:22 +0800

    首次提交

---
+++ src/components/Tabbars.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="tab-bars">
+        <el-row>
+            <el-col :span="8">
+                <div class="grid-content bg-purple-dark">123</div>
+            </el-col>
+            <el-col :span="8">
+                <div class="grid-content bg-purple-dark">123</div>
+            </el-col>
+            <el-col :span="8">
+                <div class="grid-content bg-purple-dark">123</div>
+            </el-col>
+        </el-row>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:"tabbars",
+    data(){
+        return{
+
+        }
+    },
+}
+</script>
+
+<style scoped>
+.tab-bars{
+    position:fixed;
+    bottom:0;
+    left:0;
+    width:100%;
+    height:49px;
+    text-align: center;
+}
+</style>
