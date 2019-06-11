b4cb5743f71c78e19e287b8031c2d4393bab16db
e <1303249523@qq.com>
2019-06-11 15:59:33 +0800

    底部tab

--- src/components/Tabbars.vue
+++ src/components/Tabbars.vue
@@ -2,13 +2,22 @@
     <div class="tab-bars">
         <el-row>
             <el-col :span="8">
-                <div class="grid-content bg-purple-dark">123</div>
+                <div class="grid-content bg-purple-dark icon-box">
+                    <img src="../assets/img/Ahome.png" alt=""/>
+                </div>
+                <span class="icon-title active">首页</span>
             </el-col>
             <el-col :span="8">
-                <div class="grid-content bg-purple-dark">123</div>
+                <div class="grid-content bg-purple-dark icon-box">
+                    <img src="../assets/img/order.png" alt=""/>
+                </div>
+                <span class="icon-title">订单</span>
             </el-col>
             <el-col :span="8">
-                <div class="grid-content bg-purple-dark">123</div>
+                <div class="grid-content bg-purple-dark icon-box">
+                    <img src="../assets/img/personal.png" alt=""/>
+                </div>
+                <span class="icon-title">我的</span>
             </el-col>
         </el-row>
     </div>
@@ -26,13 +35,31 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 .tab-bars{
     position:fixed;
     bottom:0;
     left:0;
     width:100%;
-    height:49px;
+    height:45pt;
     text-align: center;
+    border-top:1px solid #ddd;
+
+    .icon-box{
+        width:30pt;
+        height:30pt;
+        margin:0 auto;
+        img{
+            width:100%;
+            height:100%;
+        }
+    }
+    .icon-title{
+        font-size:10pt;
+        color:rgba(196, 196, 196, 1);
+    }
+    .active{
+        color:rgba(199, 159, 97, 1);
+    }
 }
 </style>
