623b0c7044138bb1fc3ef59a5373e1856ee793be
wangzhi <1303249523@qq.com>
2019-10-09 00:55:31 +0800

    封装日期组件

--- src/components/OrderForm/OrderFormAdd.vue
+++ src/components/OrderForm/OrderFormAdd.vue
@@ -172,6 +172,16 @@
             }
         },
         created() {
+
+            this.submitForm({
+                url: "home/option", data: {url:"/#/orderadd"}, callback: (datas) => {
+
+                    wx.config(datas.data);
+
+                }
+            });
+
+
             this.roomId = this.$route.params.id;
             console.log(this.$route.params);
             if(this.$route.params){
@@ -193,7 +203,8 @@
                         name:this.zhuName,
                         mobile:this.zhuTel,
                         more:this.userArr,
-                        type:this.isReserve
+                        type:this.isReserve,
+                        url:"/#/orderadd/"
                     };
 
                     if (this.isReserve == 1 && this.zhuIdCard != "") {
@@ -211,15 +222,13 @@
                         url: "order/add",
                         data: data,
                         callback: (data) => {
-                            console.log("userAdd",data.data);
-                            if (data.error == 0) {
 
-                                // wx.chooseWXPay(data.data);
-                                let d=data.data;
-                                console.log(d.appId,d.timeStamp);
+                            let d=data.data;
+                            console.log("order/add",parseInt(d.timestamp));
+
+                            if (data.error == 0) {
                                 wx.chooseWXPay({
-                                    appId:d.appId,
-                                    timeStamp: d.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                                    timestamp:d.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
                                     nonceStr: d.nonceStr, // 支付签名随机串，不长于 32 位
                                     package: d.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
                                     signType: d.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
@@ -228,14 +237,16 @@
                                         // 支付成功后的回调函数
                                         console.log(res);
                                         this.$Message.info("成功");
+                                        this.$router.push({path: 'homepage'})
                                     }
                                 });
+
                                 // this.$router.push({path: 'homepage'});
                             }else{
                                 this.$Message.info(data.message);
                             }
                         }
-                    })
+                    });
 
                     if (this.userArr.length > 0) {
                         let isNull = false;
@@ -244,6 +255,8 @@
                             }
                         })
                     }
+
+
                 } else {
                     this.$Message.info('请填写完整信息');
                 }
